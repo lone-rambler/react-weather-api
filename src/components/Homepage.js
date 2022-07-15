1380ba7c5a667ecda0a3d3633db36f1008417167
lone-rambler <maksymilian.wiese99@gmail.com>
2022-07-15 15:52:32 +0200

    Response processing done, better icons todo

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,6 +5,9 @@ import {
 	Subtitle,
 	LineSeparator,
 	WeatherInfoList,
+	StyledForm,
+	StyledInput,
+	StyledButton,
 } from '../styles/Style.js'
 import { useForm } from 'react-hook-form'
 import apikey from '../apikey.js'
@@ -13,6 +16,21 @@ const Homepage = () => {
 	const { register, handleSubmit } = useForm()
 	const [weatherInfo, setWeatherInfo] = useState('')
 
+	const betterIcon = (code) => {
+		let icons = ['☀️', '⛅', '☁️', '☁️', '🌧️', '🌦️', '⛈️', '❄️', '🌫️']
+		switch (code) {
+			case '01d':
+			case '01n':
+				return icons[0]
+			case '02d':
+			case '02n':
+				return icons[1]
+			case '03d':
+			case '03n':
+				return icons[2]
+		}
+	}
+
 	const onSubmit = async (data) => {
 		let location = data.location
 		const apiKey = apikey
@@ -20,29 +38,59 @@ const Homepage = () => {
 			`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric&lang=eng`
 		)
 			.then((response) => {
-				return response.json()
+				return response.ok ? response.json() : alert('An error occurred')
 			})
 			.then((data) => {
 				console.log(data)
+				let info = {
+					location: location,
+					temp: data.main.temp,
+					feels_like: data.main.feels_like,
+					description: data.weather[0].description,
+					icon: data.weather[0].icon,
+					date: new Date(data.dt * 1000).toLocaleDateString('en-EN'),
+					pressure: data.main.pressure,
+				}
+				setWeatherInfo(info)
 			})
 	}
 
 	return (
 		<FitBox>
-			<Title>Skywatcher</Title>
+			<Title>⛅Skywatcher⛈️</Title>
 			<Subtitle>Check your local weather and stay updated</Subtitle>
 			<LineSeparator />
-			<form onSubmit={handleSubmit(onSubmit)} height={'40%'}>
-				<input
-					placeholder='Szczecin, Poland'
-					{...register('locaction', { required: true })}
-				></input>
-				<button type='submit' value='Submit'>
+			<StyledForm onSubmit={handleSubmit(onSubmit)} height={'40%'}>
+				<StyledInput
+					defaultValue='Szczecin, Poland'
+					{...register('location', { required: true })}
+				></StyledInput>
+				<StyledButton type='submit' value='Submit'>
 					Check
-				</button>
-			</form>
-			<LineSeparator width='50%' />
-			<WeatherInfoList></WeatherInfoList>
+				</StyledButton>
+			</StyledForm>
+			{weatherInfo && (
+				<>
+					<WeatherInfoList>
+						<ul>
+							<li>Location: {weatherInfo.location}</li>
+							<li>Date: {weatherInfo.date}</li>
+							<li>Temperature: {weatherInfo.temp}</li>
+							<li>Feels like: {weatherInfo.feels_like}</li>
+							<li>
+								Description: {weatherInfo.description}{' '}
+								{/* <img
+									width='25'
+									height='25'
+									src={`https://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png`}
+								/> */}
+							</li>
+							<li>Pressure: {weatherInfo.pressure} Hpa</li>
+						</ul>
+					</WeatherInfoList>
+				</>
+			)}
+			<LineSeparator />
 		</FitBox>
 	)
 }
--- src/styles/Style.js
+++ src/styles/Style.js
@@ -13,13 +13,43 @@ export const GlobalStyle = createGlobalStyle`
     *{
         box-sizing: border-box;
 
-        background-color: ${colors.mint_cream};
-
         font-family: 'Gloria Hallelujah', cursive;
 
         padding: 0;
         margin: 0;
     }
+
+    html,body{
+        background-color: ${colors.mint_cream};
+    }
+
+	
+ul {
+
+list-style-type: none;
+
+}
+
+
+ul li:before {
+
+content: '';
+
+display: inline-block;
+
+height: 10px;
+
+width: 10px;
+
+ background-size: 10px;
+
+background-image: url("https://www.svgrepo.com/show/20139/checkmark-hand-drawn-outline.svg");
+
+background-repeat: no-repeat;
+
+margin-right: 5px;
+
+}
 `
 
 export const FitBox = styled.div`
@@ -39,6 +69,8 @@ export const Title = styled.div`
 	font-size: 3rem;
 	font-weight: bold;
 	text-align: center;
+
+	user-select: none;
 `
 
 export const Subtitle = styled.div`
@@ -46,6 +78,9 @@ export const Subtitle = styled.div`
 	font-size: 2rem;
 	font-weight: bold;
 	text-align: center;
+
+	user-select: none;
+	margin-bottom: 1rem;
 `
 
 export const LineSeparator = styled.div`
@@ -56,6 +91,7 @@ export const LineSeparator = styled.div`
 	border-top: 0.25rem solid ${colors.caput_mortuum};
 	border-radius: 25%;
 `
+
 export const WeatherInfoList = styled.div`
 	width: 60%;
 	height: fit-content;
@@ -66,4 +102,77 @@ export const WeatherInfoList = styled.div`
 		return props.flexDirection ? props.flexDirection : 'column'
 	}};
 	align-items: center;
+
+	margin: 0 0 1rem 0;
+`
+
+export const StyledForm = styled.form`
+	width: 50%;
+
+	display: flex;
+	justify-content: center;
+	flex-direction: row;
+
+	margin: 1rem;
+`
+
+export const StyledInput = styled.input`
+	width: 50%;
+
+	background-color: ${colors.turquoise_green};
+
+	border: none;
+	border-radius: 0.75rem;
+	outline: none;
+
+	color: ${colors.dark_sienna};
+	font-size: 1rem;
+	font-weight: bold;
+	text-align: center;
+
+	margin: 0 1rem;
+
+	transition: all 200ms ease-in;
+	-webkit-transition: all 200ms ease-in;
+	-ms-transition: all 200ms ease-in;
+
+	&:hover,
+	&:focus {
+		transform: scale(1.05);
+		background-color: ${colors.dark_sea_green};
+	}
+
+	&:-webkit-autofill {
+		-webkit-box-shadow: 0 0 0 10rem ${colors.dark_sea_green} inset; /* Change the color to your own background color */
+		-webkit-text-fill-color: ${colors.dark_sienna};
+	}
+	&:-webkit-autofill:focus {
+		-webkit-box-shadow: 0 0 0 10rem ${colors.dark_sea_green} inset; /*your box-shadow*/
+		-webkit-text-fill-color: ${colors.dark_sienna};
+	}
+`
+
+export const StyledButton = styled.button`
+	width: 25%;
+
+	background-color: ${colors.turquoise_green};
+
+	border: none;
+	border-radius: 0.75rem;
+	outline: none;
+
+	color: ${colors.dark_sienna};
+	font-size: 1rem;
+	font-weight: bold;
+	text-align: center;
+
+	user-select: none;
+	cursor: pointer;
+
+	transition: all 200ms ease-in;
+
+	&:hover {
+		transform: scale(1.05);
+		background-color: ${colors.dark_sea_green};
+	}
 `
